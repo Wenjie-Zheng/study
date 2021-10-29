42eb080de212856908925883965ca0a212f63e50
Wenjie-Zheng <1114626803@qq.com>
2021-10-29 17:56:06 +0800

    namespace

--- lib/file_stream.h
+++ lib/file_stream.h
@@ -5,6 +5,8 @@ protected:
 	char DataBuf[100];
 
 public:
+	file_stream() {
+	}
 	void file_write();
 	void file_read();
 };
--- user/main.cpp
+++ user/main.cpp
@@ -1,21 +1,48 @@
 #include <iostream>
 #include "file_stream.h"
 #include "memory.h"
+#include "fun_temp.h"
 
 using namespace std;
 
+/*命名空间定义*/
+namespace first_space {
+	void print() {
+		cout << "Inside first space" << endl;
+	}
+}
+
+namespace second_space {
+	void print() {
+		cout << "Inside second space" << endl;
+	}
+}
+
+/*函数模板*/
+template <typename test>
+inline test Max(test x, test y) {
+	return x > y ? x : y;
+}
+
 int main(void)
 {
-	memory buf;
 	file_stream file;
-
 	memory* Mbuf = new memory[5];
+
+	int a = 5, b = 3;
+	float c = 3.1, d = 4.5;
+	string e = "hello", f = "hello word";
+	cout << Max(a, b) << endl;
+	cout << Max(c, d) << endl;
+	cout << Max(e, f) << endl;
+
+	first_space::print();
+	second_space::print();
+
 	Mbuf[1].OneArray_DynamicApply(5);
 	Mbuf[2].TwoArray_DynamicApply(5, 10);
 	delete[] Mbuf;
 
-	buf.OneArray_DynamicApply(8);
-	buf.TwoArray_DynamicApply(5, 10);
 	while (true)
 	{
 		file.file_write();
