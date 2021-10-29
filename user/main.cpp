fe98019f36dc824bb59bed4c7ea66d763f0d9f0b
Wenjie-Zheng <1114626803@qq.com>
2021-10-29 14:52:08 +0800

    修改测试

--- user/main.cpp
+++ user/main.cpp
@@ -10,6 +10,9 @@ int main(void)
 	while (true)
 	{
 		cout << "first modify" << endl; 
+		cout << "first modify" << endl;
+		cout << "first modify" << endl;
+		cout << "ÐÞ¸ÄÒ»´Î²âÊÔ" << endl;
 		file.file_write();
 		file.file_read();
 	}
